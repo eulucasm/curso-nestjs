1b31d3975b4a300a1e926805a7472a1f6d3ca9fa
eulucasm <lucaspoletis@gmail.com>
2021-10-06 15:42:53 -0300

    persistencia de dados com Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://lucaspoletislinux:ZaK6K2LjQsV5uo6e@cluster0.obba6.mongodb.net/smartranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      }),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
