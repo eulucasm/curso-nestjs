c96d2ebd93385e90fc0b4eadeab493069cc9dca9
eulucasm <lucaspoletis@gmail.com>
2021-10-08 16:53:54 -0300

    Implementação da entidade categorias e primeiro teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://lucaspoletislinux:ZaK6K2LjQsV5uo6e@cluster0.obba6.mongodb.net/smartranking?retryWrites=true&w=majority'),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
